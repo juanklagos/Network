9927099ab2146fbb5f68148b67ff614e14355ad7
Juan Carlos Alvarez Lagos <jcalvarez31@misena.edu.co>
2020-08-03 22:47:57 -0500

    commit

--- config.php
+++ config.php
@@ -3,22 +3,23 @@
 // | @author Deen Doughouz (DoughouzForest)
 // | @author_url 1: http://www.wowonder.com
 // | @author_url 2: http://codecanyon.net/user/doughouzforest
-// | @author_email: wowondersocial@gmail.com   
+// | @author_email: wowondersocial@gmail.com
 // +------------------------------------------------------------------------+
 // | WoWonder - The Ultimate PHP Social Networking Platform
-// | Copyright (c) 2017 WoWonder. All rights reserved.
+// | Copyright (c) 2016 WoWonder. All rights reserved.
 // +------------------------------------------------------------------------+
-@header('Location: install');
-exit();
 // MySQL Hostname
-$sql_db_host = "db_name";
+$sql_db_host = "128.199.0.209";
 // MySQL Database User
-$sql_db_user = "db_user";
+$sql_db_user = "root";
 // MySQL Database Password
-$sql_db_pass = "db_pass";
+$sql_db_pass = "ssiiASS@@@aa123";
 // MySQL Database Name
-$sql_db_name = "db_name";
+$sql_db_name = "network";
 
 // Site URL
-$site_url = "site_url"; // e.g (http://example.com)
+$site_url = "https://diosesbueno.app"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "9dfc0f22-3577-422d-96e7-0dffe2a56b4c"; // Your purchase code, don't give it to anyone.
 ?>
